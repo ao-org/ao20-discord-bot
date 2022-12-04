9292bd25ae03ec5999274a6b9b77cd9a4ce7b3b1
Recox <lucas.recoaro@gmail.com>
2022-12-04 20:04:31 +1300

    Cambio https por http en link a estadisticas

--- src/commands/reporte.js
+++ src/commands/reporte.js
@@ -1,6 +1,6 @@
 const { SuccessEmbed, ErrorEmbed } = require("../embeds");
 const axios = require('axios');
-const url = 'https://estadisticas.ao20.com.ar/produccion/reports.php?last=true&dir=reports'
+const url = 'http://estadisticas.ao20.com.ar/produccion/reports.php?last=true&dir=reports'
 
 async function getLastReport() {
   const auth = {
@@ -23,7 +23,7 @@ async function sendReport(channel, data) {
     new SuccessEmbed()
       .setColor(0x3f0e3e)
       .setTitle("Oro total del mundo (no incluye valor de items)")
-      .setURL('https://estadisticas.ao20.com.ar/produccion/reports.php?dir=reports')
+      .setURL('http://estadisticas.ao20.com.ar/produccion/reports.php?dir=reports')
       .addFields({ name: 'Inflacion', value: report.data.Gold })
       .setTimestamp()
       .setFooter(footer)
@@ -35,7 +35,7 @@ async function sendReport(channel, data) {
         new SuccessEmbed()
           .setColor(report.data.Reports[user].Errors ? 0xf91d09 : 0xc9ea10)
           .setTitle(`${user}`)
-          .setURL('https://estadisticas.ao20.com.ar/produccion/reports.php?dir=reports')
+          .setURL('http://estadisticas.ao20.com.ar/produccion/reports.php?dir=reports')
           .addFields(
             { name: 'AccId', value: report.data.Reports[user].AccID, inline: true },
             { name: 'BaseLevel', value: report.data.Reports[user].BaseLevel, inline: true },
@@ -55,7 +55,7 @@ async function sendReport(channel, data) {
         new SuccessEmbed()
           .setColor(0xf44ede)
           .setTitle(`Cuenta ID: ${report.data.AccountReports[account].AccID}`)
-          .setURL('https://estadisticas.ao20.com.ar/produccion/reports.php?dir=reports')
+          .setURL('http://estadisticas.ao20.com.ar/produccion/reports.php?dir=reports')
           .addFields({ name: 'Warnings', value: report.data.AccountReports[account].Warnings ? report.data.AccountReports[account].Warnings : 'No hay warnings' })
           .addFields({ name: 'Errors', value: report.data.AccountReports[account].Errors ? report.data.AccountReports[account].Errors : 'No hay errores' })
           .setTimestamp()
